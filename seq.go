63ffaa3255ce1eb47e44e29e42ad28431f55abf0
Jason Song <i@wolfogre.com>
2025-10-21 17:41:08 +0800

    refactor: rename to point

--- seq.go
+++ seq.go
@@ -7,18 +7,18 @@ import (
 	"time"
 )
 
-type Item[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64] struct {
+type Point[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64] struct {
 	Time  time.Time
 	Value T
 }
 
-func (i Item[T]) IsZero() bool {
+func (i Point[T]) IsZero() bool {
 	var zero T
 	return i.Time.IsZero() && i.Value == zero
 }
 
 type Seq[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64] struct {
-	slice []Item[T]
+	points []Point[T]
 
 	indexOnce  sync.Once
 	timeIndex  map[timeKey][]int
@@ -26,24 +26,24 @@ type Seq[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8
 	valueOrder []int
 }
 
-// NewSeq returns Seq with copied slice inside
-func NewSeq[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64](slice []Item[T]) *Seq[T] {
-	temp := make([]Item[T], len(slice))
-	copy(temp, slice)
+// NewSeq returns Seq with copied points inside
+func NewSeq[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64](points []Point[T]) *Seq[T] {
+	temp := make([]Point[T], len(points))
+	copy(temp, points)
 	return WrapSeq(temp)
 }
 
-// WrapSeq returns Seq with origin slice inside
-func WrapSeq[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64](slice []Item[T]) *Seq[T] {
-	if !slices.IsSortedFunc(slice, compareItems[T]) {
-		slices.SortStableFunc(slice, compareItems[T])
+// WrapSeq returns Seq with origin points inside
+func WrapSeq[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64](points []Point[T]) *Seq[T] {
+	if !slices.IsSortedFunc(points, compareItems[T]) {
+		slices.SortStableFunc(points, compareItems[T])
 	}
 	return &Seq[T]{
-		slice: slice,
+		points: points,
 	}
 }
 
-func compareItems[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64](a, b Item[T]) int {
+func compareItems[T float32 | float64 | int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64](a, b Point[T]) int {
 	if a.Time.Before(b.Time) {
 		return -1
 	}
@@ -58,17 +58,17 @@ func (s *Seq[T]) buildIndex() {
 		return
 	}
 	s.indexOnce.Do(func() {
-		timeIndex := make(map[timeKey][]int, len(s.slice))
-		valueIndex := make(map[T][]int, len(s.slice))
+		timeIndex := make(map[timeKey][]int, len(s.points))
+		valueIndex := make(map[T][]int, len(s.points))
 		valueSlice := s.valueOrder[:0]
-		for i, v := range s.slice {
+		for i, v := range s.points {
 			k := newTimeKey(v.Time)
 			timeIndex[k] = append(timeIndex[k], i)
 			valueIndex[v.Value] = append(valueIndex[v.Value], i)
 			valueSlice = append(valueSlice, i)
 		}
 		sort.SliceStable(valueSlice, func(i, j int) bool {
-			return s.slice[valueSlice[i]].Value < s.slice[valueSlice[j]].Value
+			return s.points[valueSlice[i]].Value < s.points[valueSlice[j]].Value
 		})
 		s.timeIndex = timeIndex
 		s.valueIndex = valueIndex
@@ -76,75 +76,75 @@ func (s *Seq[T]) buildIndex() {
 	})
 }
 
-// Slice returns a replica of inside slice
-func (s *Seq[T]) Slice() []Item[T] {
-	ret := make([]Item[T], len(s.slice))
-	copy(ret, s.slice)
+// Slice returns a replica of inside points
+func (s *Seq[T]) Slice() []Point[T] {
+	ret := make([]Point[T], len(s.points))
+	copy(ret, s.points)
 	return ret
 }
 
-// Len returns length of inside slice
+// Len returns length of inside points
 func (s *Seq[T]) Len() int {
-	return len(s.slice)
+	return len(s.points)
 }
 
-// Index returns element of inside slice, returns zero if index is out of range
-func (s *Seq[T]) Index(i int) Item[T] {
-	if i < 0 || i >= len(s.slice) {
-		return Item[T]{}
+// Index returns point of inside points, returns zero if index is out of range
+func (s *Seq[T]) Index(i int) Point[T] {
+	if i < 0 || i >= len(s.points) {
+		return Point[T]{}
 	}
-	return s.slice[i]
+	return s.points[i]
 }
 
-// Time returns the first element with time t, returns zero if not found
-func (s *Seq[T]) Time(t time.Time) Item[T] {
+// Time returns the first point with time t, returns zero if not found
+func (s *Seq[T]) Time(t time.Time) Point[T] {
 	got := s.MTime(t)
 	if len(got) == 0 {
-		return Item[T]{}
+		return Point[T]{}
 	}
 	return got[0]
 }
 
-// MTime returns all elements with time t, returns nil if not found
-func (s *Seq[T]) MTime(t time.Time) []Item[T] {
+// MTime returns all points with time t, returns nil if not found
+func (s *Seq[T]) MTime(t time.Time) []Point[T] {
 	s.buildIndex()
 	index := s.timeIndex[newTimeKey(t)]
 	if len(index) == 0 {
 		return nil
 	}
-	ret := make([]Item[T], len(index))
+	ret := make([]Point[T], len(index))
 	for i, v := range index {
-		ret[i] = s.slice[v]
+		ret[i] = s.points[v]
 	}
 	return ret
 }
 
-// Value returns the first element with value v, returns zero if not found
-func (s *Seq[T]) Value(v T) Item[T] {
+// Value returns the first point with value v, returns zero if not found
+func (s *Seq[T]) Value(v T) Point[T] {
 	got := s.MValue(v)
 	if len(got) == 0 {
-		return Item[T]{}
+		return Point[T]{}
 	}
 	return got[0]
 }
 
-// MValue returns all elements with value v, returns nil if not found
-func (s *Seq[T]) MValue(v T) []Item[T] {
+// MValue returns all points with value v, returns nil if not found
+func (s *Seq[T]) MValue(v T) []Point[T] {
 	s.buildIndex()
 	index := s.valueIndex[v]
 	if len(index) == 0 {
 		return nil
 	}
-	ret := make([]Item[T], len(index))
+	ret := make([]Point[T], len(index))
 	for i, v := range index {
-		ret[i] = s.slice[v]
+		ret[i] = s.points[v]
 	}
 	return ret
 }
 
-// Traverse call fn for every element one by one, break if fn returns false
-func (s *Seq[T]) Traverse(fn func(i int, v Item[T]) (stop bool)) {
-	for i, v := range s.slice {
+// Traverse call fn for every point one by one, break if fn returns false
+func (s *Seq[T]) Traverse(fn func(i int, v Point[T]) (stop bool)) {
+	for i, v := range s.points {
 		if !fn(i, v) {
 			break
 		}
@@ -154,16 +154,16 @@ func (s *Seq[T]) Traverse(fn func(i int, v Item[T]) (stop bool)) {
 // Sum returns sum of all values
 func (s *Seq[T]) Sum() T {
 	var ret T
-	for _, v := range s.slice {
+	for _, v := range s.points {
 		ret += v.Value
 	}
 	return ret
 }
 
-// Max returns the element with max value, returns zero if empty
-func (s *Seq[T]) Max() Item[T] {
-	var ret Item[T]
-	for _, v := range s.slice {
+// Max returns the point with max value, returns zero if empty
+func (s *Seq[T]) Max() Point[T] {
+	var ret Point[T]
+	for _, v := range s.points {
 		if ret.IsZero() {
 			ret = v
 		} else if v.Value > ret.Value {
@@ -173,12 +173,12 @@ func (s *Seq[T]) Max() Item[T] {
 	return ret
 }
 
-// Min returns the element with min value, returns zero if empty
+// Min returns the point with min value, returns zero if empty
 func (s *Float32Seq) Min() Float32 {
 	var min Float32
 	found := false
-	sslice := s.getSlice()
-	for _, v := range sslice {
+	spoints := s.getSlice()
+	for _, v := range spoints {
 		if !found {
 			min = v
 			found = true
@@ -189,30 +189,30 @@ func (s *Float32Seq) Min() Float32 {
 	return min
 }
 
-// First returns the first element, returns zero if empty
+// First returns the first point, returns zero if empty
 func (s *Float32Seq) First() Float32 {
-	sslice := s.getSlice()
-	if len(sslice) == 0 {
+	spoints := s.getSlice()
+	if len(spoints) == 0 {
 		return Float32{}
 	}
-	return sslice[0]
+	return spoints[0]
 }
 
-// Last returns the last element, returns zero if empty
+// Last returns the last point, returns zero if empty
 func (s *Float32Seq) Last() Float32 {
-	sslice := s.getSlice()
-	if len(sslice) == 0 {
+	spoints := s.getSlice()
+	if len(spoints) == 0 {
 		return Float32{}
 	}
-	return sslice[len(sslice)-1]
+	return spoints[len(spoints)-1]
 }
 
-// Percentile returns the element matched with percentile pct, returns zero if empty,
+// Percentile returns the point matched with percentile pct, returns zero if empty,
 // the pct's valid range is be [0, 1], it will be treated as 1 if greater than 1, as 0 if smaller than 0
 func (s *Float32Seq) Percentile(pct float64) Float32 {
 	s.buildIndex()
-	sslice := s.getSlice()
-	if len(sslice) == 0 {
+	spoints := s.getSlice()
+	if len(spoints) == 0 {
 		return Float32{}
 	}
 	if pct > 1 {
@@ -221,18 +221,18 @@ func (s *Float32Seq) Percentile(pct float64) Float32 {
 	if pct < 0 {
 		pct = 0
 	}
-	i := int(float64(len(sslice))*pct - 1)
+	i := int(float64(len(spoints))*pct - 1)
 	if i < 0 {
 		i = 0
 	}
-	return sslice[s.valueOrder[i]]
+	return spoints[s.valueOrder[i]]
 }
 
 // Range returns a sub *Float32Seq with specified interval
 func (s *Float32Seq) Range(interval Interval) *Float32Seq {
-	sslice := s.getSlice()
-	slice := Range(sslice, interval).(Float32s)
-	return newFloat32Seq(slice)
+	spoints := s.getSlice()
+	points := Range(spoints, interval).(Float32s)
+	return newFloat32Seq(points)
 }
 
 // Slice returns a sub *Float32Seq with specified index,
@@ -241,25 +241,25 @@ func (s *Float32Seq) Slice(i, j int) *Float32Seq {
 	if i < 0 && j < 0 {
 		return s
 	}
-	sslice := s.getSlice()
+	spoints := s.getSlice()
 	if i < 0 {
-		return newFloat32Seq(sslice[:j])
+		return newFloat32Seq(spoints[:j])
 	}
 	if j < 0 {
-		return newFloat32Seq(sslice[i:])
+		return newFloat32Seq(spoints[i:])
 	}
-	return newFloat32Seq(sslice[i:j])
+	return newFloat32Seq(spoints[i:j])
 }
 
-// Trim returns a *Float32Seq without elements which make fn returns true
+// Trim returns a *Float32Seq without points which make fn returns true
 func (s *Float32Seq) Trim(fn func(i int, v Float32) bool) *Float32Seq {
-	sslice := s.getSlice()
-	if fn == nil || len(sslice) == 0 {
+	spoints := s.getSlice()
+	if fn == nil || len(spoints) == 0 {
 		return s
 	}
 
 	removeM := map[int]struct{}{}
-	for i, v := range sslice {
+	for i, v := range spoints {
 		if fn(i, v) {
 			removeM[i] = struct{}{}
 		}
@@ -268,15 +268,15 @@ func (s *Float32Seq) Trim(fn func(i int, v Float32) bool) *Float32Seq {
 		return s
 	}
 
-	slice := make(Float32s, 0, len(sslice)-len(removeM))
-	for i, v := range sslice {
+	points := make(Float32s, 0, len(spoints)-len(removeM))
+	for i, v := range spoints {
 		if _, ok := removeM[i]; ok {
 			continue
 		}
-		slice = append(slice, v)
+		points = append(points, v)
 	}
 
-	return newFloat32Seq(slice)
+	return newFloat32Seq(points)
 }
 
 // Merge returns a new *Float32Seq with merged data according to the specified rule
@@ -287,43 +287,43 @@ func (s *Float32Seq) Merge(fn func(t time.Time, v1, v2 *float32) *float32, seq *
 
 	var ret Float32s
 
-	slice1 := s.getSlice()
-	var slice2 Float32s
+	points1 := s.getSlice()
+	var points2 Float32s
 	if seq != nil {
-		slice2 = seq.getSlice()
+		points2 = seq.getSlice()
 	}
 
-	for i1, i2 := 0, 0; i1 < len(slice1) || i2 < len(slice2); {
+	for i1, i2 := 0, 0; i1 < len(points1) || i2 < len(points2); {
 		var (
 			t time.Time
 			v *float32
 		)
 		switch {
-		case i1 == len(slice1):
-			t = slice2[i2].Time
-			v2 := slice2[i2].Value
+		case i1 == len(points1):
+			t = points2[i2].Time
+			v2 := points2[i2].Value
 			v = fn(t, nil, &v2)
 			i2++
-		case i2 == len(slice2):
-			t = slice1[i1].Time
-			v1 := slice1[i1].Value
+		case i2 == len(points2):
+			t = points1[i1].Time
+			v1 := points1[i1].Value
 			v = fn(t, &v1, nil)
 			i1++
-		case slice1[i1].Time.Equal(slice2[i2].Time):
-			t = slice1[i1].Time
-			v1 := slice1[i1].Value
-			v2 := slice2[i2].Value
+		case points1[i1].Time.Equal(points2[i2].Time):
+			t = points1[i1].Time
+			v1 := points1[i1].Value
+			v2 := points2[i2].Value
 			v = fn(t, &v1, &v2)
 			i1++
 			i2++
-		case slice1[i1].Time.Before(slice2[i2].Time):
-			t = slice1[i1].Time
-			v1 := slice1[i1].Value
+		case points1[i1].Time.Before(points2[i2].Time):
+			t = points1[i1].Time
+			v1 := points1[i1].Value
 			v = fn(t, &v1, nil)
 			i1++
-		case slice1[i1].Time.After(slice2[i2].Time):
-			t = slice2[i2].Time
-			v2 := slice2[i2].Value
+		case points1[i1].Time.After(points2[i2].Time):
+			t = points2[i2].Time
+			v2 := points2[i2].Value
 			v = fn(t, nil, &v2)
 			i2++
 		}
@@ -339,7 +339,7 @@ func (s *Float32Seq) Merge(fn func(t time.Time, v1, v2 *float32) *float32, seq *
 }
 
 // Aggregate returns a aggregated *Float32Seq according to the specified rule
-func (s *Float32Seq) Aggregate(fn func(t time.Time, slice Float32s) *float32, duration time.Duration, interval Interval) *Float32Seq {
+func (s *Float32Seq) Aggregate(fn func(t time.Time, points Float32s) *float32, duration time.Duration, interval Interval) *Float32Seq {
 	if fn == nil {
 		return s
 	}
@@ -347,17 +347,17 @@ func (s *Float32Seq) Aggregate(fn func(t time.Time, slice Float32s) *float32, du
 	ret := Float32s{}
 	temp := Float32s{}
 
-	sslice := s.getSlice()
+	spoints := s.getSlice()
 	if duration <= 0 {
 		for i := 0; i < s.Len(); {
-			t := sslice[i].Time
+			t := spoints[i].Time
 			if !interval.Contain(t) {
 				i++
 				continue
 			}
 			temp = temp[:0]
-			for i < sslice.Len() && t.Equal(sslice[i].Time) {
-				temp = append(temp, sslice[i])
+			for i < spoints.Len() && t.Equal(spoints[i].Time) {
+				temp = append(temp, spoints[i])
 				i++
 			}
 			v := fn(t, temp)
@@ -371,13 +371,13 @@ func (s *Float32Seq) Aggregate(fn func(t time.Time, slice Float32s) *float32, du
 		return newFloat32Seq(ret)
 	}
 
-	if len(sslice) == 0 && interval.Duration() < 0 {
+	if len(spoints) == 0 && interval.Duration() < 0 {
 		return s
 	}
 
 	var begin time.Time
-	if len(sslice) > 0 {
-		begin = sslice[0].Time.Truncate(duration)
+	if len(spoints) > 0 {
+		begin = spoints[0].Time.Truncate(duration)
 	}
 	if interval.NotBefore != nil {
 		begin = (*interval.NotBefore).Truncate(duration)
@@ -387,8 +387,8 @@ func (s *Float32Seq) Aggregate(fn func(t time.Time, slice Float32s) *float32, du
 	}
 
 	var end time.Time
-	if len(sslice) > 0 {
-		end = sslice[len(sslice)-1].Time.Truncate(duration)
+	if len(spoints) > 0 {
+		end = spoints[len(spoints)-1].Time.Truncate(duration)
 	}
 	if interval.NotAfter != nil {
 		end = (*interval.NotAfter).Truncate(duration)
@@ -397,12 +397,12 @@ func (s *Float32Seq) Aggregate(fn func(t time.Time, slice Float32s) *float32, du
 	for t, i := begin, 0; !t.After(end); t = t.Add(duration) {
 		temp = temp[:0]
 		itv := BeginAt(t).EndAt(t.Add(duration))
-		for i < len(sslice) {
-			if sslice[i].Time.After(*itv.NotAfter) {
+		for i < len(spoints) {
+			if spoints[i].Time.After(*itv.NotAfter) {
 				break
 			}
-			if itv.Contain(sslice[i].Time) {
-				temp = append(temp, sslice[i])
+			if itv.Contain(spoints[i].Time) {
+				temp = append(temp, spoints[i])
 			}
 			i++
 		}
